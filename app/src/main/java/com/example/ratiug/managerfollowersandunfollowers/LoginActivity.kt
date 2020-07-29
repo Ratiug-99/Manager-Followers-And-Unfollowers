dc27e83766b290f7327254b711cbf48454ade87e
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-07-29 12:44:23 +0300

    Create request uri auth

--- app/src/main/java/com/example/ratiug/managerfollowersandunfollowers/LoginActivity.kt
+++ app/src/main/java/com/example/ratiug/managerfollowersandunfollowers/LoginActivity.kt
@@ -4,8 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var appId: String
+    private lateinit var appSecret: String
+    private lateinit var redirectURI: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        appId = getString(R.string.instagram_app_id)
+        appSecret = getString(R.string.instagram_secret)
+        redirectURI = getString(R.string.instagram_redirect_uri)
+
     }
+
+    val mAuthUrl = ("https://api.instagram.com/oauth/authorize"
+            + "?client_id=$appId"
+            + "&redirect_uri=$redirectURI"
+            + "&scope=user_profile,user_media"
+            + "&response_type=code")
+
 }
